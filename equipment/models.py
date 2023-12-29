0052285fee7f8c18618257fd203eb63461e01862
aysema mermer <aysemamermer99@gmail.com>
2023-12-30 00:23:10 +0300

    first commit

---
+++ equipment/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+from django.db import models
+from machines.models import Machine
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=255)
+    inventory_number = models.CharField(max_length=50, unique=True)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
