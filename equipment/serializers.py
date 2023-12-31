b76a7be7a1c6f73f0dc6f2c1e9fd911817d617fa
aysema mermer <aysemamermer99@gmail.com>
2024-01-01 01:50:35 +0300

    Happy new year: Equipment filter and put

--- equipment/serializers.py
+++ equipment/serializers.py
@@ -1,30 +1,29 @@
 from rest_framework import serializers
-from .models import Equipment
+from .models import Equipment, Machine
 
 class EquipmentSerializer(serializers.ModelSerializer):
+    machine_id = serializers.PrimaryKeyRelatedField(
+        queryset=Machine.objects.all(), source='machine', required=False
+    )
+
     class Meta:
         model = Equipment
-        fields = ['id', 'name', 'inventory_number', 'machine', 'created_at']
+        fields = ['id', 'name', 'inventory_number', 'machine_id', 'created_at']
 
-    def validate_inventory_number(self, value):
-        if self.instance and self.instance.inventory_number == value:
-            raise serializers.ValidationError("Inventory number must be unique.")
-
-        if Equipment.objects.filter(inventory_number=value).exists():
-            raise serializers.ValidationError("Another equipment with this inventory number already exists.")
-
-        return value
+    # ...
 
     def create(self, validated_data):
-        instance = super().create(validated_data)
-        self.add_success_message(instance, created=True)
-        return instance
+        machine_id = validated_data.pop('machine_id', None)
+        equipment = super().create(validated_data)
+        if machine_id:
+            equipment.machine_id = machine_id
+            equipment.save()
+        return equipment
 
     def update(self, instance, validated_data):
+        machine_id = validated_data.pop('machine_id', None)
         instance = super().update(instance, validated_data)
-        self.add_success_message(instance, created=False)
+        if machine_id:
+            instance.machine_id = machine_id
+            instance.save()
         return instance
-
-    def add_success_message(self, instance, created):
-        success_message = "Equipment created successfully!" if created else "Equipment updated successfully!"
-        setattr(self.context['view'], 'success_message', success_message)
--- equipment/views.py
+++ equipment/views.py
@@ -5,17 +5,27 @@ from .serializers import EquipmentSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-
 class EquipmentListCreateView(generics.ListCreateAPIView):
-    queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
+
+    def get_queryset(self):
+        queryset = Equipment.objects.all()
+
+        name = self.request.query_params.get('name', None)
+        inventory_number = self.request.query_params.get('inventory_number', None)
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        if inventory_number:
+            queryset = queryset.filter(inventory_number__icontains=inventory_number)
+
+        return queryset
 
 
 class EquipmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
-
 
 class EquipmentDeleteView(APIView):
     def delete(self, request, pk):
@@ -23,7 +33,6 @@ class EquipmentDeleteView(APIView):
         # İzin kontrolü ve silme işlemi burada gerçekleştirilebilir
         equipment.delete()
         return Response({'message': 'Equipment deleted successfully.'}, status=status.HTTP_204_NO_CONTENT)
-
 
 # views.py
 class EquipmentView(APIView):
